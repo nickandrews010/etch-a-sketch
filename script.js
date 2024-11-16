5f4b5f9fac3d21d3898f80eda906582416eb8249
Nick <nickandrews010@gmail.com>
2024-11-16 12:19:23 -0800

    Create 16x16 grid using flexbox

---
+++ script.js
@@ -0,0 +1,7 @@
+let container = document.getElementById("container");
+
+for (let i = 0; i < (16 * 16); i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+}
