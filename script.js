d4584bdb93dab83f29bc2ff925f14e74cb4d7da1
Nick <nickandrews010@gmail.com>
2024-11-16 17:07:26 -0800

    Fix bug that made color changes temporary

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ for (let i = 0; i < (16 * 16); i++) {
     let square = document.createElement("div");
     square.classList.add("square");
     container.appendChild(square);
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "red";
+    });
 }
 
 button.addEventListener("click", () => {
     let num = +prompt("Enter the number of squares you want, per side, in the new grid. (1-100)");
+    if (num > 100) {
+        alert("The number must be 100 or less.  Please try again.");
+        return;
+    }
     container.innerHTML = "";
 
     for (let i = 0; i < (num * num); i++) {
@@ -16,5 +23,8 @@ button.addEventListener("click", () => {
         square.classList.add("newSquare");
         square.style.flex =  `1 1 ${100 / num}%`;
         container.appendChild(square);
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "red";
+        });
     }
 })
